a1f17877f33fb6f7d0be2396d80959e29869e6dc
Dylan Araps <dylan.araps@gmail.com>
2017-06-17 00:50:27 +1000

    General: Initial files

--- wal.py
+++ wal.py
@@ -3,6 +3,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(description='wal - Generate colorschemes on the fly')
+
     parser.add_argument('-a', help='Set terminal background transparency. *Only works in URxvt*', metavar='0-100', type=int)
     parser.add_argument('-c', help='Delete all cached colorschemes.', action='store_true')
     parser.add_argument('-f', help='Load colors directly from a colorscheme file.', metavar='"/path/to/colors"')
@@ -13,6 +14,7 @@ def get_args():
     parser.add_argument('-r', help='Reload current colorscheme.', action='store_true')
     parser.add_argument('-t', help='Fix artifacts in VTE Terminals. (Termite, xfce4-terminal)', action='store_true')
     parser.add_argument('-x', help='Use extended 16-color palette.', action='store_true')
+
     args = parser.parse_args()
     return args
 
