9adecb1bc31c40e6173927342bf3209d164b0fa6
Dylan Araps <dylan.araps@gmail.com>
2017-06-20 20:42:47 +1000

    General: Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""wal - setup.py"""
+from setuptools import setup
+
+setup(
+    name="wal",
+    version="0.1",
+    author="Dylan Araps",
+    author_email="dylan.araps@gmail.com",
+    description="🎨 Generate and change colorschemes on the fly",
+    license="MIT",
+    url="https://github.com/dylanaraps/wal.py",
+    scripts=["wal"]
+)
