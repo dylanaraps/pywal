58f30bed4d45b9e9ea46e1d0310d80002ca20b2b
Dylan Araps <dylan.araps@gmail.com>
2017-06-22 14:29:26 +1000

    pypi: Fix README

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 """wal - setup.py"""
 from setuptools import setup
 
+
+try:
+    import pypandoc
+    DESC = pypandoc.convert('README.md', 'rst')
+    DESC = DESC.replace("\r", "")
+except(IOError, ImportError):
+    DESC = open('README.md').read()
+
+
 setup(
     name="pywal",
-    version="0.1",
+    version="0.1.0",
     author="Dylan Araps",
     author_email="dylan.araps@gmail.com",
     description="🎨 Generate and change colorschemes on the fly",
+    long_description=DESC,
     license="MIT",
     url="https://github.com/dylanaraps/wal.py",
+    download_url="https://github.com/dylanaraps/wal.py/archive/0.1.0.tar.gz",
     scripts=["wal"]
 )
