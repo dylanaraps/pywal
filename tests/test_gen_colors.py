2cee85a15ed50a7ef3fc94b4fb47ba4d5cb44a51
Dylan Araps <dylan.araps@gmail.com>
2017-06-27 17:43:14 +1000

    tests: Add a real image file.

--- tests/test_gen_colors.py
+++ tests/test_gen_colors.py
@@ -17,6 +17,11 @@ class TestGenColors(unittest.TestCase):
         result = gen_colors.get_image("tests/test_files")
         self.assertEqual(result, "tests/test_files/test.jpg")
 
+    def test_gen_colors(self):
+        """> Generate a colorscheme."""
+        result = gen_colors.gen_colors("tests/test_files/test.jpg")
+        self.assertEqual(result[0], "#0F191A")
+
 
 if __name__ == "__main__":
     unittest.main()
