930c3c8e40ca5c563d0414e5057b23ad27aa7cf3
Dylan Araps <dylan.araps@gmail.com>
2017-06-30 10:10:07 +1000

    tests: Update tests.

--- tests/test_gen_colors.py
+++ tests/test_gen_colors.py
@@ -7,16 +7,6 @@ from pywal import gen_colors
 class TestGenColors(unittest.TestCase):
     """Test the gen_colors functions."""
 
-    def test_get_img(self):
-        """> Validate image file."""
-        result = gen_colors.get_image("tests/test_files/test.jpg")
-        self.assertEqual(result, "tests/test_files/test.jpg")
-
-    def test_get_img_dir(self):
-        """> Validate image directory."""
-        result = gen_colors.get_image("tests/test_files")
-        self.assertEqual(result, "tests/test_files/test.jpg")
-
     def test_gen_colors(self):
         """> Generate a colorscheme."""
         result = gen_colors.gen_colors("tests/test_files/test.jpg")
