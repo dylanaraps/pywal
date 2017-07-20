6678fa7953fde67d7371a28f6ddfe5b1aae03cdc
dylan araps <dylan.araps@gmail.com>
2017-07-20 10:44:21 +1000

    general: Add example script.

---
+++ examples/example.py
@@ -0,0 +1,22 @@
+"""pywal api example."""
+import pywal
+
+
+def main():
+    """Main function."""
+    image = "/home/dylan/Pictures/Wallpapers/anVmEaA.jpg"
+
+    # Return a dict with the palette.
+    colors = pywal.create_palette(image)
+
+    # Apply the palette to all open terminals.
+    # Second argument is a boolean for VTE terminals.
+    # Set it to true if the terminal you're using is
+    # VTE based. (xfce4-terminal, termite, gnome-terminal.)
+    pywal.send_sequences(colors, False)
+
+    # Reload xrdb, i3 and polybar.
+    pywal.reload_env()
+
+    # Set the wallpaper.
+    pywal.set_wallpaper(image)
