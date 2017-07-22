3dd1fc9039822e4426d5dc8c3f807dd5f6f2d295
Dylan Araps <dylan.araps@gmail.com>
2017-07-22 10:26:36 +1000

    tests: Fix tests

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,20 +1,24 @@
 """Test image functions."""
+import pathlib
 import unittest
 
 from pywal import wal
+
+
+DEVNULL = pathlib.Path("/dev/null")
 
 
 class TestImage(unittest.TestCase):
     """Test image functions."""
     def test_get_img(self):
         """> Validate image file."""
-        result = wal.get_image("tests/test_files/test.jpg")
+        result = wal.get_image("tests/test_files/test.jpg", DEVNULL)
         self.assertEqual(result, "tests/test_files/test.jpg")
 
     def test_get_img_dir(self):
         """> Validate image directory."""
-        result = wal.get_image("tests/test_files")
-        self.assertEqual(result, "tests/test_files/test2.jpg")
+        result = wal.get_image("tests/test_files", DEVNULL)
+        self.assertEqual(result, "tests/test_files/test.jpg")
 
 
 if __name__ == "__main__":
