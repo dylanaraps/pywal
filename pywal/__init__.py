6e39b78dc3a8ae6fc164f2fe26f6c0004d92dc41
Dylan Araps <dylan.araps@gmail.com>
2017-07-22 17:46:40 +1000

    general: Use __all__ in __init__.py

--- pywal/__init__.py
+++ pywal/__init__.py
@@ -2,7 +2,6 @@
 wal - Generate and change colorschemes on the fly.
 Created by Dylan Araps.
 """
-# flake8: noqa: F401
 from pywal.wal import __version__
 from pywal.wal import create_palette
 from pywal.wal import export_all_templates
@@ -11,3 +10,14 @@ from pywal.wal import reload_colors
 from pywal.wal import reload_env
 from pywal.wal import send_sequences
 from pywal.wal import set_wallpaper
+
+__all__ = [
+    "__version__",
+    "create_palette",
+    "export_all_templates",
+    "get_image",
+    "reload_colors",
+    "reload_env",
+    "send_sequences",
+    "set_wallpaper",
+]
