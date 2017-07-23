0ccc55ffdd2aeb824b077a3b7c2d273a904edeeb
Dylan Araps <dylan.araps@gmail.com>
2017-07-23 21:42:30 +1000

    tests: Test json file with no wallpaper.

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -40,6 +40,11 @@ class TestGenColors(unittest.TestCase):
         result = colors.file("tests/test_files/test_file.json")
         self.assertEqual(result["colors"]["color0"], "#1F211E")
 
+    def test_color_import_no_wallpaper(self):
+        """> Read colors from a file without a wallpaper."""
+        result = colors.file("tests/test_files/test_file2.json")
+        self.assertEqual(result["wallpaper"], "None")
+
 
 if __name__ == "__main__":
     unittest.main()
