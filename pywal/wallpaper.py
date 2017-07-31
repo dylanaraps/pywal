d8293b20de34cb7c36bc39438cb574554af2b361
Dylan Araps <dylan.araps@gmail.com>
2017-07-31 16:07:54 +1000

    wallpaper: Correct sqlite3 command.

--- pywal/wallpaper.py
+++ pywal/wallpaper.py
@@ -84,7 +84,7 @@ def set_desktop_wallpaper(desktop, img):
 def set_mac_wallpaper(img):
     """Set the wallpaper on macOS."""
     db_file = HOME / "Library/Application Support/Dock/desktoppicture.db"
-    subprocess.call(["sqlite3", str(db_file), f"update data set value = {img}"])
+    subprocess.call(["sqlite3", db_file, f"update data set value = '{img}'"])
 
     # Kill the dock to fix issues with wallpapers sharing names.
     util.disown("killall", "Dock")
