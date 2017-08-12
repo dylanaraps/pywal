c743cab4f0b74e928496c2a5052906da52acc8f7
Dylan Araps <dylan.araps@gmail.com>
2017-08-12 12:21:22 +1000

    tests: Add a test for sequence order.

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -34,6 +34,13 @@ class Testsequences(unittest.TestCase):
         result = sequences.set_iterm_tab_color(COLORS["special"]["background"])
         self.assertEqual(len(result), 104)
 
+    def test_sequence_order(self):
+        """> Test that the sequences are in order."""
+        result = sequences.create_sequences(COLORS, vte=False).split("\007")
+        self.assertEqual(result[2], "\x1b]4;2;#CC6A93")
+        self.assertEqual(result[15], "\x1b]4;15;#F5F1F4")
+        self.assertEqual(result[8], "\x1b]4;8;#666666")
+
 
 if __name__ == "__main__":
     unittest.main()
