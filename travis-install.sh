bd2c2539941aa837d71905b66e5034491eaf6e2f
Matthew Leonberger <linuxunil@gmail.com>
2017-09-11 17:04:55 +0900

    travis-ci has some bugs with osx python install from brew

--- travis-install.sh
+++ travis-install.sh
@@ -2,6 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install imagemagick
+    brew install python && brew link --overwite python
 else
    sudo apt-get -qq update
    sudo apt-get install -y imagemagick
