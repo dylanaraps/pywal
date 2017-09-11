ce85481631ae20829bf6363274319105757288b0
Matthew Leonberger <linuxunil@gmail.com>
2017-09-11 18:15:18 +0900

    flake8 and pylint can run for git hooks
    Make travis do parallel and add a script for install.
    Merge remote-tracking branch 'origin/travis-cleanup' into travis-cleanup
    
    
    Merge remote-tracking branch 'origin/travis-cleanup' into travis-cleanup
    
    Merge remote-tracking branch 'origin/travis-cleanup' into travis-cleanup
    
    
    travis-ci has some bugs with osx python install from brew
    
    MacOS shouldn't need a 3.5 build but needs homebrew python
    
    An experement with travic-ci and before_install
    
    An experement with travic-ci and before_install
    
    Merge remote-tracking branch 'origin/travis-cleanup' into travis-cleanup
    An experement with travic-ci and before_install
    
    Merge remote-tracking branch 'origin/travis-cleanup' into travis-cleanup
    
    Travis-CI update for macos
    
    Fixed issues with python on macos and set up matrix builds.  Currently linux takes very little time.  MacOS significantly longer due to homebrew wanting to update.
    
    Revert "Travis-CI update for macos"
    
    This reverts commit 31b4a13a1f72c2b0fc58de952f58bd49c37edfb8.

---
+++ travis-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    #brew install imagemagick
+    #brew install python && brew link --overwite python3
+else
+   sudo apt-get -qq update
+   sudo apt-get install -y imagemagick
+fi
