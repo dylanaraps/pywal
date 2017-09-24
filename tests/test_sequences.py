a56019761246857ede1a0e702324ad0274ff001f
Matthew Leonberger <linuxunil@gmail.com>
2017-09-24 16:48:02 +0900

    Add OS detection for mac specific tests
    Merge branch 'macos_test' of https://github.com/linuxunil/pywal into macos_test
    
    Revert "pylint and flake8 need to be in before_install"
    
    This reverts commit b2320013e737ed9048b3946cdfee4f2ee113d62d.

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -1,5 +1,6 @@
 """Test sequence functions."""
 import unittest
+import platform
 
 from pywal import sequences
 from pywal import util
@@ -16,23 +17,33 @@ class Testsequences(unittest.TestCase):
         """> Create special escape sequence."""
         util.Color.alpha_num = 100
         result = sequences.set_special(11, COLORS["special"]["background"])
-        self.assertEqual(result, "\033]11;#1F211E\007")
+        if platform.uname()[0] == "Darwin":
+            self.assertEqual(result, "\033]Ph1F211E\033\\")
+        else:
+            self.assertEqual(result, "\033]11;#1F211E\007")
 
     def test_set_special_alpha(self):
         """> Create special escape sequence with alpha."""
         util.Color.alpha_num = 99
         result = sequences.set_special(11, COLORS["special"]["background"])
-        self.assertEqual(result, "\033]11;[99]#1F211E\007")
+        if platform.uname()[0] == "Darwin":
+            self.assertEqual(result, "\033]Ph1F211E\033\\")
+        else:
+            self.assertEqual(result, "\033]11;[99]#1F211E\007")
 
     def test_set_color(self):
         """> Create color escape sequence."""
         result = sequences.set_color(11, COLORS["colors"]["color0"])
-        self.assertEqual(result, "\033]4;11;#1F211E\007")
+        if platform.uname()[0] == "Darwin":
+            self.assertEqual(result, "\033]Pb1F211E\033\\")
+        else:
+            self.assertEqual(result, "\033]4;11;#1F211E\007")
 
     def test_set_iterm_tab_color(self):
         """> Create iterm tab color sequences"""
         result = sequences.set_iterm_tab_color(COLORS["special"]["background"])
         self.assertEqual(len(result), 104)
+
 
 if __name__ == "__main__":
     unittest.main()
