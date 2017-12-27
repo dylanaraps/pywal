72a7713ca8415f22198ff6abdb60b9ab44f4e974
Dylan Araps <dylan.araps@gmail.com>
2017-12-27 15:57:36 +1100

    templates: Fix bug

--- pywal/templates/colors-wal-dwm.h
+++ pywal/templates/colors-wal-dwm.h
@@ -12,7 +12,7 @@ static const char urg_border[] = "{color1}";
 
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
-    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-    [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+    [SchemeNorm] = {{ norm_fg,     norm_bg,   norm_border }}, // unfocused wins
+    [SchemeSel]  = {{ sel_fg,      sel_bg,    sel_border }},  // the focused win
+    [SchemeUrg] =  {{ urg_fg,      urg_bg,    urg_border }},
 }};
