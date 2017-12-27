3f1b6e5946d624dcb178dbddd1566a54d1d028aa
Dylan Araps <dylan.araps@gmail.com>
2017-12-27 15:22:25 +1100

    templates: Add template for tabbed.

---
+++ pywal/templates/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "{color0}";
+static const char* selfgcolor   = "{color15}";
+static const char* normbgcolor  = "{color2}";
+static const char* normfgcolor  = "{color15}";
+static const char* urgbgcolor   = "{color1}";
+static const char* urgfgcolor   = "{color15}";
