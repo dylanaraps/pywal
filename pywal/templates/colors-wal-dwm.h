f96ecf248269f8f289b7d3cb7acf89d031a856d9
Dylan Araps <dylan.araps@gmail.com>
2017-12-27 15:25:03 +1100

    templates: Add template for dwm.

---
+++ pywal/templates/colors-wal-dwm.h
@@ -0,0 +1,18 @@
+static const char norm_fg[] = "{color15}";
+static const char norm_bg[] = "{color0}";
+static const char norm_border[] = "{color8}";
+
+static const char sel_fg[] = "{color15}";
+static const char sel_bg[] = "{color2}";
+static const char sel_border[] = "{color15}";
+
+static const char urg_fg[] = "{color15}";
+static const char urg_bg[] = "{color1}";
+static const char urg_border[] = "{color1}";
+
+static const char *colors[][3]      = {
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+    [SchemeUrg] =  { urg_fg,      urg_bg,    urg_border },
+};
