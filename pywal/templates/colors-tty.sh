9dede6dd61584b1e3bc6a92d022f2ad2b88e873a
Dylan Araps <dylan.araps@gmail.com>
2018-02-12 14:38:56 +1100

    templates: Update tty.sh

--- pywal/templates/colors-tty.sh
+++ pywal/templates/colors-tty.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
-if [ "${{TERM:-none}}" = "linux" ]; then
-    printf "%b" "\\e]P0{color0.strip}"
-    printf "%b" "\\e]P1{color1.strip}"
-    printf "%b" "\\e]P2{color2.strip}"
-    printf "%b" "\\e]P3{color3.strip}"
-    printf "%b" "\\e]P4{color4.strip}"
-    printf "%b" "\\e]P5{color5.strip}"
-    printf "%b" "\\e]P6{color6.strip}"
-    printf "%b" "\\e]P7{color7.strip}"
-    printf "%b" "\\e]P8{color8.strip}"
-    printf "%b" "\\e]P9{color9.strip}"
-    printf "%b" "\\e]PA{color10.strip}"
-    printf "%b" "\\e]PB{color11.strip}"
-    printf "%b" "\\e]PC{color12.strip}"
-    printf "%b" "\\e]PD{color13.strip}"
-    printf "%b" "\\e]PE{color14.strip}"
-    printf "%b" "\\e]PF{color15.strip}"
-
-    # Fix artifacting.
-    clear
-fi
+[ "${{TERM:-none}}" = "linux" ] && \
+    printf '%b' '\e]P0{color0.strip}
+                 \e]P1{color1.strip}
+                 \e]P2{color2.strip}
+                 \e]P3{color3.strip}
+                 \e]P4{color4.strip}
+                 \e]P5{color5.strip}
+                 \e]P6{color6.strip}
+                 \e]P7{color7.strip}
+                 \e]P8{color8.strip}
+                 \e]P9{color9.strip}
+                 \e]PA{color10.strip}
+                 \e]PB{color11.strip}
+                 \e]PC{color12.strip}
+                 \e]PD{color13.strip}
+                 \e]PE{color14.strip}
+                 \e]PF{color15.strip}
+                 \ec'
