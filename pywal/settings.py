82a571db3fe059803d276c97df0c7d0b911dd059
Dylan Araps <dylan.araps@gmail.com>
2018-04-01 14:15:58 +1000

    config: cleanup

--- pywal/settings.py
+++ pywal/settings.py
@@ -17,11 +17,13 @@ import platform
 __version__ = "1.3.3"
 __cache_version__ = "1.0.0"
 
+
 HOME = os.getenv("HOME", os.getenv("USERPROFILE"))
 CACHE_DIR = os.path.join(HOME, ".cache", "wal")
 MODULE_DIR = os.path.dirname(__file__)
 CONF_DIR = os.path.join(HOME, ".config", "wal")
 OS = platform.uname()[0]
+
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read("/home/black/.config/wal/config.ini")
