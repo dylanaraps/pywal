a6a067eec557730d0de875002d6ad73bdd2ab807
Dylan Araps <dylan.araps@gmail.com>
2018-05-28 07:57:58 +1000

    general: Set LS_COLORS to fix unreadable output. Closes #248

--- pywal/templates/colors.sh
+++ pywal/templates/colors.sh
@@ -31,3 +31,6 @@ export FZF_DEFAULT_OPTS="
     --color fg:7,bg:0,hl:1,fg+:232,bg+:1,hl+:255
     --color info:7,prompt:2,spinner:1,pointer:232,marker:1
 "
+
+# Fix LS_COLORS being unreadable.
+export LS_COLORS="$LS_COLORS:su=30;41:ow=30;42:st=30;44:"
