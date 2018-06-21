0e9d68aa2a490753209f85d447572fd0c10684e9
Dylan Araps <dylan.araps@gmail.com>
2018-06-21 10:51:11 +1000

    colors: fix test

--- pywal/colors.py
+++ pywal/colors.py
@@ -75,7 +75,7 @@ def generic_adjust(colors, light):
 
 def saturate_colors(colors, amount):
     """Saturate all colors."""
-    if float(amount) <= 1.0:
+    if amount and float(amount) <= 1.0:
         for i, _ in enumerate(colors):
             if i not in [0, 7, 8, 15]:
                 colors[i] = util.saturate_color(colors[i], float(amount))
