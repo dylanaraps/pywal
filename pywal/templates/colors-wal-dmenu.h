041fe0643f9a31079cdf641a7f8227e97dba9696
Younes Khoudli <younes.khoudli@epita.fr>
2018-12-30 03:40:11 +0100

    template: add dmenu template

---
+++ pywal/templates/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {{
+	/*     fg         bg       */
+	[SchemeNorm] = {{ "{color15}", "{color0}" }},
+	[SchemeSel] = {{ "{color15}", "{color1}" }},
+	[SchemeOut] = {{ "{color15}", "{color14}" }},
+}};
