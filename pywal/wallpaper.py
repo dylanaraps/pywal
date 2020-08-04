c3faf0e1cf4600551f9fe979d265b04ee7cef62d
threadreaper <threadreaper@gmail.com>
2020-08-04 03:15:26 -0500

    Errors fixed.

--- pywal/wallpaper.py
+++ pywal/wallpaper.py
@@ -121,11 +121,11 @@ def set_desktop_wallpaper(desktop, img):
         string = """
             var allDesktops = desktops();for (i=0;i<allDesktops.length;i++){
             d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";
-            d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
-            d.writeConfig("Image", "%s")};
+            d.currentConfigGroup = Array("Wallpaper", "org.kde.image",
+            "General");d.writeConfig("Image", "%s")};
         """
-        util.disown(["qdbus", "org.kde.plasmashell", "/PlasmaShell", "org.kde.PlasmaShell.evaluateScript",
-                    string % img])
+        util.disown(["qdbus", "org.kde.plasmashell", "/PlasmaShell",
+                     "org.kde.PlasmaShell.evaluateScript", string % img])
     else:
         set_wm_wallpaper(img)
 
