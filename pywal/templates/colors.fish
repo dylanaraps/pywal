03bcb52a40ee65f3d8221ad0456d5ff45b3839a7
Mysy00 <pm@mysy.pl>
2020-12-01 00:52:54 +0100

    Add fish shell template

---
+++ pywal/templates/colors.fish
@@ -0,0 +1,25 @@
+set fish_color_normal normal
+set fish_color_command {color1.strip}
+set fish_color_param {color5.strip}
+set fish_color_redirection $fish_color_param
+set fish_color_comment {color8.strip}
+set fish_color_error ff0000
+set fish_color_escape {color5.strip}
+set fish_color_operator $fish_color_escape
+set fish_color_end {color2.strip}
+set fish_color_quote {color2.strip}
+set fish_color_autosuggestion 555 brblack
+set fish_color_user brgreen
+set fish_color_host $fish_color_normal
+set fish_color_valid_path --underline
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_match --background=brblue
+set fish_color_search_match bryellow --background=brblack
+set fish_color_selection white --bold --background=brblack
+set fish_color_cancel -r
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_completion
+set fish_pager_color_description $fish_color_quote yellow
+set fish_pager_color_progress brwhite --background=cyan
+set fish_color_history_current --bold
