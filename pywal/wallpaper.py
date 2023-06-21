16e0aadbf5dcb36a8943a060e250999d08f0f579
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-06-21 00:00:35 +0000

    Added swaybg support

--- pywal/wallpaper.py
+++ pywal/wallpaper.py
@@ -59,7 +59,11 @@ def xfconf(img):
 
 def set_wm_wallpaper(img):
     """Set the wallpaper for non desktop environments."""
-    if shutil.which("feh"):
+    if shutil.which("swayb"):
+        subprocess.call(["killall", "swaybg"])
+        util.disown(["swaybg", "-m", "fill", "-i", img])
+
+    elif shutil.which("feh"):
         util.disown(["feh", "--bg-fill", img])
 
     elif shutil.which("xwallpaper"):
